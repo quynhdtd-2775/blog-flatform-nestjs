2d9955fb9cb78c346019a7974d0469d8743c92f2
quynhdtd-2775 <doan.thi.diem.quynh@sun-asterisk.com>
2026-03-02 09:45:01 +0700

    chore: add migration

---
+++ src/database/1772417292280-migrations.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Migrations1772417292280 implements MigrationInterface {
+  name = 'Migrations1772417292280';
+
+  // eslint-disable-next-line @typescript-eslint/require-await, @typescript-eslint/no-unused-vars
+  public async up(_queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await, @typescript-eslint/no-unused-vars
+  public async down(_queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+}
